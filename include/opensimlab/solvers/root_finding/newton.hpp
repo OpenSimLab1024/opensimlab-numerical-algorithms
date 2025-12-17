57e534b1de0f10a6e7c25a268e1003d9710306a9
OpenSimLab <opensimlab1024@gmail.com>
2025-12-17 23:32:59 +0100

    Add Newton method public API

--- include/opensimlab/solvers/root_finding/newton.hpp
+++ include/opensimlab/solvers/root_finding/newton.hpp
@@ -1,1 +1,33 @@
-// newton.hpp
+#pragma once
+
+#include <functional>
+#include <cstddef>
+
+namespace opensimlab {
+namespace solvers {
+namespace root_finding {
+
+/**
+ * @brief Solve f(x) = 0 using Newton's method.
+ *
+ * @param f         Function f(x)
+ * @param df        Derivative f'(x)
+ * @param x0        Initial guess
+ * @param tol       Convergence tolerance
+ * @param max_iter  Maximum number of iterations
+ * @return double   Approximated root
+ *
+ * @throws std::runtime_error if convergence fails
+ */
+double newton(
+    const std::function<double(double)>& f,
+    const std::function<double(double)>& df,
+    double x0,
+    double tol = 1e-8,
+    std::size_t max_iter = 100
+);
+
+} // namespace root_finding
+} // namespace solvers
+} // namespace opensimlab
+
